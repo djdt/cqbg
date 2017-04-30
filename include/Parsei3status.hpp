1e83b501c07467af0f4fb51dc8f347517c04dee8
djdt <djdt@djdt.djdt>
2017-04-30 18:10:08 +1000

    removed parsing, is done in bash now

--- include/Parsei3status.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef _PARSE_i3_STATUS_H_
-#define _PARSE_i3_STATUS_H_
-
-#include "Base16Colors.hpp"
-
-void parsei3StatusConfig(const std::string& infile, const std::string& outfile,
-		const std::vector<std::string>& colors);
-
-#endif /* ifndef _PARSE_i3_STATUS_H_ */
